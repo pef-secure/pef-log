0929331f672ecd512f55e98583adc466cb648b18
pef-secure <pef-secure@yandex.ru>
2015-04-30 20:12:58 +0200

    skip itself

--- lib/PEF/Log/OverAppender.pm
+++ lib/PEF/Log/OverAppender.pm
@@ -36,7 +36,7 @@ APS
 	}
 	$app_sub .= <<APS;
 		my \$of = \$PEF::Log::caller_offset;
-		local \$PEF::Log::caller_offset = \$of + 1;
+		local \$PEF::Log::caller_offset = \$of + 2;
 		\$self->{appender}->append(\$level, \$stream, \$msg);
 	}
 APS
